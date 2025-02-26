74c43692e952d6a945751a531df2e0dfd27bef23
Bhavesh Patil <167158628+bhaveshpatil093@users.noreply.github.com>
2025-02-26 19:24:52 +0530

    Create deploy.ts

---
+++ scripts/deploy.ts
@@ -0,0 +1,11 @@
+import {web3} from 'hardhat'
+import artifacts from '../artifacts/contracts/Lock.sol/Lock.json'
+
+async function main() {
+    
+}
+
+main().catch(error => {
+    console.error(error);
+    process.exitCode = 1;
+});
